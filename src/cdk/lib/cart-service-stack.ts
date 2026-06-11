02de17972b64f9802175ce22b1a2e5af67c789f4
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-06-11 23:29:54 +0400

    App wrapped

---
+++ src/cdk/lib/cart-service-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+
+export class CartServiceStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cartLambda = new lambda.Function(this, 'CartServiceLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'dist/lambda.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../')),
+      timeout: cdk.Duration.seconds(30),
+      environment: {
+        DB_HOST: process.env.DB_HOST || '',
+        DB_PORT: process.env.DB_PORT || '5432',
+        DB_NAME: process.env.DB_NAME || '',
+        DB_USER: process.env.DB_USER || '',
+        DB_PASSWORD: process.env.DB_PASSWORD || '',
+      },
+    });
+
+    new apigateway.LambdaRestApi(this, 'CartServiceApi', {
+      handler: cartLambda,
+      proxy: true,
+    });
+  }
+}
